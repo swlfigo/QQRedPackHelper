2a2c550c442a3b3dad4ec8506b5c9422ae85cfe0
tangxianhai <>
2018-03-03 17:42:11 +0800

    增加文字口令红包功能

--- QQRedPackHelper/QQRedPackHelper.m
+++ QQRedPackHelper/QQRedPackHelper.m
@@ -23,37 +23,37 @@
 @class RedPackWindowController;
 @class RedPackViewController;
 
-static void (*origin_TChatWalletTransferViewController_updateUI)(TChatWalletTransferViewController *,SEL);
-static void new_TChatWalletTransferViewController_updateUI(TChatWalletTransferViewController* self,SEL _cmd) {
-    origin_TChatWalletTransferViewController_updateUI(self,_cmd);
-
-    if ([[QQHelperSetting sharedInstance] isEnableRedPacket]) {
-        id chatWalletVc = self;
-        id chatWalletTransferViewModel = [chatWalletVc valueForKey:@"_viewModel"];
-        if (chatWalletTransferViewModel) {
-            id helperRedPackViewMode = [chatWalletTransferViewModel valueForKey:@"_redPackViewModel"];
-            // 判读显示的单条消息是否红包
-            if (helperRedPackViewMode) {
-                NSDictionary *helperRedPackDic = [helperRedPackViewMode valueForKey:@"_redPackDic"];
-                id chatWalletContentView = [chatWalletVc valueForKey:@"_walletContentView"];
-                if (chatWalletContentView) {
-                    // 判断红包本机是否抢过
-                    id helperRedPackOpenStateText = [chatWalletVc valueForKey:@"_redPackOpenStateLabel"];
-                    if (helperRedPackOpenStateText) {
-                        NSString *redPackOpenState = [helperRedPackOpenStateText performSelector:@selector(stringValue)];
-                        if (![redPackOpenState isEqualToString:@"已拆开"]) {
-                            NSLog(@"QQRedPackHelper：抢到红包 - 红包信息: %@",helperRedPackDic);
-                            [chatWalletContentView performSelector:@selector(performClick)];
-                            [QQHelperNotification showNotificationWithTitle:@"红包助手提示" content:@"抢到红包😝😝😝"];
-                        } else {
-                            NSLog(@"QQRedPackHelper：检测到历史红包 - 红包信息: %@",helperRedPackDic);
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
+//static void (*origin_TChatWalletTransferViewController_updateUI)(TChatWalletTransferViewController *,SEL);
+//static void new_TChatWalletTransferViewController_updateUI(TChatWalletTransferViewController* self,SEL _cmd) {
+//    origin_TChatWalletTransferViewController_updateUI(self,_cmd);
+//
+//    if ([[QQHelperSetting sharedInstance] isEnableRedPacket]) {
+//        id chatWalletVc = self;
+//        id chatWalletTransferViewModel = [chatWalletVc valueForKey:@"_viewModel"];
+//        if (chatWalletTransferViewModel) {
+//            id helperRedPackViewMode = [chatWalletTransferViewModel valueForKey:@"_redPackViewModel"];
+//            // 判读显示的单条消息是否红包
+//            if (helperRedPackViewMode) {
+//                NSDictionary *helperRedPackDic = [helperRedPackViewMode valueForKey:@"_redPackDic"];
+//                id chatWalletContentView = [chatWalletVc valueForKey:@"_walletContentView"];
+//                if (chatWalletContentView) {
+//                    // 判断红包本机是否抢过
+//                    id helperRedPackOpenStateText = [chatWalletVc valueForKey:@"_redPackOpenStateLabel"];
+//                    if (helperRedPackOpenStateText) {
+//                        NSString *redPackOpenState = [helperRedPackOpenStateText performSelector:@selector(stringValue)];
+//                        if (![redPackOpenState isEqualToString:@"已拆开"]) {
+//                            NSLog(@"QQRedPackHelper：抢到红包 - 红包信息: %@",helperRedPackDic);
+//                            [chatWalletContentView performSelector:@selector(performClick)];
+//                            [QQHelperNotification showNotificationWithTitle:@"红包助手提示" content:@"抢到红包😝😝😝"];
+//                        } else {
+//                            NSLog(@"QQRedPackHelper：检测到历史红包 - 红包信息: %@",helperRedPackDic);
+//                        }
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
 static void (*origin_MQAIOChatViewController_handleAppendNewMsg)(MQAIOChatViewController *,SEL,id);
 static void new_MQAIOChatViewController_handleAppendNewMsg(MQAIOChatViewController* self,SEL _cmd,id msg) {
@@ -62,35 +62,24 @@ static void new_MQAIOChatViewController_handleAppendNewMsg(MQAIOChatViewControll
     [chatWalletVc performSelector:@selector(didClickNewMsgRemindPerformButton)];
 }
 
-//static void (*origin_BHMsgListManager_onRefreshUnreadCountWhenGetMessage)(BHMsgListManager *,SEL,NSNotification *);
-//static void new_BHMsgListManager_onRefreshUnreadCountWhenGetMessage(BHMsgListManager* self,SEL _cmd, NSNotification * msgNotification) {
-//    origin_BHMsgListManager_onRefreshUnreadCountWhenGetMessage(self,_cmd,msgNotification);
-//    if ([[QQHelperSetting sharedInstance] isEnableRedPacket]) {
-//        // 获取最新消息
-//        NSDictionary *redPacketDic = [msgNotification userInfo];
-//        NSArray *messages = [redPacketDic objectForKey:@"msgArray"];
-//        if ([messages count] != 0) {
-//            for (id message in messages) {
-//                id redPackHelper = NSClassFromString(@"RedPackHelper");
-//                if ([message isKindOfClass:NSClassFromString(@"BHMessageModel")]) {
-//                    int mType = [[message valueForKey:@"_msgType"] intValue];
-//                    if (mType == 311) {
-//                        // 红包消息
-//
-//                        id content = [message performSelector:@selector(content)];
-//
-//                        id traceManager = [NSClassFromString(@"TXTraceManager") performSelector:@selector(shareInstance)];
-//                        [traceManager performSelector:@selector(setBreadcrumb:argument:) withObject:@"BC_OPEN_REDPACK_MOUSE_DOWN" withObject:@""];
-//
-//                        [redPackHelper performSelector:@selector(openRedPackWithMsgModel:operation:) withObject:message withObject:@(0)];
-//
-//                        NSLog(@"QQRedPackHelper：抢到红包 %@ ---- 详细信息: %@",message,content);
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+static id (*origin_BHMsgListManager_getMessageKey)(BHMsgListManager *,SEL,id);
+static id new_BHMsgListManager_getMessageKey(BHMsgListManager* self,SEL _cmd, id msgKey) {
+    id key = origin_BHMsgListManager_getMessageKey(self,_cmd,msgKey);
+    if ([[QQHelperSetting sharedInstance] isEnableRedPacket]) {
+        id redPackHelper = NSClassFromString(@"RedPackHelper");
+        if ([msgKey isKindOfClass:NSClassFromString(@"BHMessageModel")]) {
+            int mType = [[msgKey valueForKey:@"_msgType"] intValue];
+            if (mType == 311) {
+                // 红包消息
+                [redPackHelper performSelector:@selector(openRedPackWithMsgModel:operation:) withObject:msgKey withObject:@(0)];
+                id content = [msgKey performSelector:@selector(content)];
+                [QQHelperNotification showNotificationWithTitle:@"红包助手提示" content:@"抢到红包😝😝😝"];
+                NSLog(@"QQRedPackHelper：抢到红包 %@ ---- 详细信息: %@",msgKey,content);
+            }
+        }
+    }
+    return key;
+}
 
 static void (*origin_AppController_applicationDidFinishLaunching)(AppController *,SEL,NSNotification *);
 static void new_AppController_applicationDidFinishLaunching(AppController* self,SEL _cmd,NSNotification * aNotification) {
@@ -134,15 +123,13 @@ static void __attribute__((constructor)) initialize(void) {
     // 助手设置菜单
     MSHookMessageEx(objc_getClass("AppController"), @selector(applicationDidFinishLaunching:), (IMP)&new_AppController_applicationDidFinishLaunching, (IMP *)&origin_AppController_applicationDidFinishLaunching);
     
-    // 模拟抢红包 - 通用
-    MSHookMessageEx(objc_getClass("TChatWalletTransferViewController"), @selector(_updateUI), (IMP)&new_TChatWalletTransferViewController_updateUI, (IMP *)&origin_TChatWalletTransferViewController_updateUI);
+    // 模拟抢红包 - 通用 - 比较慢，每次刷新UI都要变化弹框 弃用
+//    MSHookMessageEx(objc_getClass("TChatWalletTransferViewController"), @selector(_updateUI), (IMP)&new_TChatWalletTransferViewController_updateUI, (IMP *)&origin_TChatWalletTransferViewController_updateUI);
     
     // 消息滚到底部 - 才会自动刷新UI
     MSHookMessageEx(objc_getClass("MQAIOChatViewController"), @selector(handleAppendNewMsg:), (IMP)&new_MQAIOChatViewController_handleAppendNewMsg, (IMP *)&origin_MQAIOChatViewController_handleAppendNewMsg);
     
-    /* 方法弃用，有问题。
-    MSHookMessageEx(objc_getClass("BHMsgListManager"), @selector(onGetMessage:), (IMP)&new_BHMsgListManager_onRefreshUnreadCountWhenGetMessage, (IMP *)&origin_BHMsgListManager_onRefreshUnreadCountWhenGetMessage);
-     */
-    
+    // 模拟抢红包方式二，底层调用
      MSHookMessageEx(objc_getClass("RedPackViewController"), @selector(viewDidLoad), (IMP)&new_RedPackViewController_viewDidLoad, (IMP *)&origin_RedPackViewController_viewDidLoad);
+    MSHookMessageEx(objc_getClass("BHMsgListManager"), @selector(getMessageKey:), (IMP)&new_BHMsgListManager_getMessageKey, (IMP *)&origin_BHMsgListManager_getMessageKey);
 }
